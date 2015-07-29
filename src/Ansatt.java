ac323062cce7ca819c3c6aef5a5c363ce645a764
Tine Kleivane <kleivane@gmail.com>
2015-07-29 22:09:42 +0200

    Navn: Legg til ansatt for å demonstrere navngivingsissues

---
+++ src/Ansatt.java
@@ -0,0 +1,30 @@
+public class Ansatt {
+
+    private String fornavnString;
+    private String etternavnString;
+    private String ssnNumber;
+    private int mob;
+
+    public Ansatt(String ssn, String fornavn, String etternavn){
+        this.ssnNumber = ssn;
+        this.fornavnString = fornavn;
+        this.etternavnString = etternavn;
+    }
+
+    public String ansattnavn(){
+        return this.fornavnString + " " +  this.etternavnString;
+    }
+
+    public void setMob(int mob) {
+        this.mob = mob;
+    }
+
+    public static void main(String[] args) {
+        Ansatt nora = new Ansatt("29028409638", "Nora", "Hansen");
+        nora.setMob(23357701);
+        System.out.println(nora.ansattnavn());
+    }
+
+
+
+}
